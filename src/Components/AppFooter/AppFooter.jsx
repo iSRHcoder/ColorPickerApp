0a84a26276477973f65870c7b84d77d057b581cd
iSRHcoder <s.r.hatkadke@gmail.com>
2023-11-09 17:30:03 +0530

    updated footer

--- src/Components/AppFooter/AppFooter.jsx
+++ src/Components/AppFooter/AppFooter.jsx
@@ -1,9 +1,18 @@
 import styles from "./AppFooter.module.css";
+import { FaGithub } from "react-icons/fa";
 
 const AppFooter = () => {
   return (
     <>
-      <div className={styles.AppFooter}>`Made with ❤️ By iSRHcoder [2023]`</div>
+      <div className={styles.AppFooter}>
+        <div> `Made with ❤️ By iSRHcoder [2023]`</div>
+        <div>
+          {" "}
+          <a href="https://github.com/iSRHcoder" style={{ color: "white" }}>
+            <FaGithub />
+          </a>
+        </div>
+      </div>
     </>
   );
 };
